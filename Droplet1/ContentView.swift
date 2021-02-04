f22e010aa72510652cbcfaf8ec44efa44d2f04f8
Schanen Ryan <schanen.r@gmail.com>
2021-02-03 17:51:40 -0800

    Opened project and added Droplet model to AR scene

--- Droplet1/ContentView.swift
+++ Droplet1/ContentView.swift
@@ -21,16 +21,17 @@ struct ARViewContainer: UIViewRepresentable {
         let arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let dropletAnchor = try! ModelProjectDroplet.loadScene()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.anchors.append(dropletAnchor)
         
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
+    
     
 }
 
