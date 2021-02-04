b0a7bfbe238a05cf67fbf6da123a53f2079e669f
Schanen Ryan <schanen.r@gmail.com>
2021-02-04 15:22:10 -0800

    Added Podfile

--- Droplet1/AppDelegate.swift
+++ Droplet1/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftUI
 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
 
--- Droplet1/ContentView.swift
+++ Droplet1/ContentView.swift
@@ -15,34 +15,6 @@ struct ContentView : View {
     }
 }
 
-//struct ARViewContainer: UIViewRepresentable {
-//    
-//    func makeUIView(context: Context) -> ARView {
-//        
-//        let arView = ARView(frame: .zero)
-//        
-//        // Load the "Box" scene from the "Experience" Reality File
-//        let dropletAnchor = try! ModelProjectDroplet.loadScene()
-//        
-//        // Add the box anchor to the scene
-//        arView.scene.addAnchor(dropletAnchor)
-//        
-//        dropletAnchor.actions.dropletTapped.onAction = handleTapOnEntity(_:)
-//
-//        print("scene created")
-//        return arView
-//        
-//    }
-//    
-//    func updateUIView(_ uiView: ARView, context: Context){
-//    }
-//    
-//    func handleTapOnEntity(_: Entity?) {
-//        print("What up?")
-//    }
-//
-//}
-
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
