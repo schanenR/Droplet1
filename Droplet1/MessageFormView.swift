42a3f28e064fa1c5f94f247918ddc5419c0294ed
Schanen Ryan <schanen.r@gmail.com>
2021-02-06 13:53:24 -0800

    MessageFormView in the works.

--- Droplet1/MessageFormView.swift
+++ Droplet1/MessageFormView.swift
@@ -5,20 +5,32 @@
 //  Created by Ada on 2/5/21.
 //
 
-//import SwiftUI
-//
-//
-//struct MessageFormView: View {
-//
-////    @ObservedObject var userMessage: Message
-//
-//    var body: some View {
-//        Text(GoogleMapsView.userMessage.message!)
-//    }
-//}
-//
-//struct MessageFormView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MessageFormView()
-//    }
-//}
+import SwiftUI
+
+
+struct MessageFormView: View {
+    
+    @State private var text = ""
+
+        var body: some View {
+            ZStack {
+                Image("droplet-notext")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.top)
+                VStack {
+                    TextField("Type message here...", text: $text)
+                      .padding(10)
+                        .frame( width: 300, height: 200)
+                      .font(Font.system(size: 20, weight: .medium, design: .serif))
+                      .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 1))
+                }
+            }
+        }
+}
+
+struct MessageFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageFormView()
+    }
+}
